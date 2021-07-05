a1eb182cd94f140657dac894e1fb082973ba69fe
ortamfag <a_zhidov@outlook.com>
2021-07-05 17:09:03 +0300

    ADD: connected handlebars

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const exphbs = require('express-handlebars')
 const portfolioRoutes = require('./routes/portfolio')
 
 const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
