d37cfa2de592fed1dfb9776105bf4ab3b698e01a
ortamfag <a_zhidov@outlook.com>
2021-07-06 19:33:00 +0300

    ADD: model

---
+++ models/Portfolio.js
@@ -0,0 +1,37 @@
+const {Schema, model} = require('mongoose')
+
+const schema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    group: {
+        type: String,
+        required: true
+    },
+
+    faculty: {
+        type: String,
+        required: true
+    },
+
+    customer: {
+        type: String
+    },
+
+    curator: {
+        type: String
+    },
+
+    team: {
+        type: String
+    },
+
+    laboratory: {
+        type: String
+    }
+
+})
+
+module.exports = model('Portfolio', schema)
